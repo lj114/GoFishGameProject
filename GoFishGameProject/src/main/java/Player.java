ee28516fa5990677e9bfe1353c90064e7a515b51
lj114 <l-jennings@wiu.edu>
2022-04-10 19:27:52 -0500

    Update Player.java

--- GoFishGameProject/src/main/java/Player.java
+++ GoFishGameProject/src/main/java/Player.java
@@ -11,7 +11,14 @@ public class Player {
     protected boolean turn; //Tracks if it is the player's turn. 
     
     public Player(){
+        hand = new ArrayList<Card>();
         pairs = 0;
+        turn = false;
+    }
+    
+    //Set turn to true or false.
+    public void setTurn(boolean t){
+        turn = t;
     }
     
     //Returns true or false, to check if it is the Player's turn.
@@ -29,19 +36,26 @@ public class Player {
         hand.add(c);
     }
     
-    //Remove a specified Card from the player's hand.
-    public void removeCardfromHand(Card c){
+    //Remove a specified Card from the player's hand and returns it.
+    public Card removeCardfromHand(Card c){
         int check = hand.indexOf(c);    //Checks if the card is in the player's hand. 
         if(check == -1){
             System.out.println("Card is not in player's hand, cannot be removed.");
+            return null;
         }
         else{
             hand.remove(check);
+            return c;
         }  
     }
     
     //Add 1 to pairs.
     public void addPair(){
         pairs++;
+    }
+    
+    //Returns number of pairs the player has made.
+    public int getPairs(){
+        return pairs;
     }
 }
